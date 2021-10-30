ce35b13b0bc0308db306c4f326621d362eab098e
Julien Moutte <julien.moutte@bentley.com>
2021-10-30 22:31:27 +0200

    Implement turn_off and update

--- pump.py
+++ pump.py
@@ -1,8 +1,11 @@
+import time
+
 class Pump:
     power = None
     name = None
     desired_runtime = 0
     runtime = 0
+    on_since = None
     
     def __init__(self, name, power, runtime):
         self.power = power
@@ -15,15 +18,25 @@ class Pump:
     def can_run(self, available_power):
         return available_power >= self.power
     
+    def is_running(self):
+        return self.on_since != None
+    
     def turn_on(self):
         print(f'Starting pump {self.name}')
+        # Call GPIO to turn the pump on
+        self.on_since = time.time()
+    
+    def turn_off(self):
+        ran_for = time.time() - self.on_since
+        print(f'Stopping pump {self.name}, ran for {ran_for} seconds')
+        # Call GPIO to turn the pump off
+        self.runtime += ran_for
+        self.on_since = None
 
     def update(self):
-        # Check if the pump is currently running
+        if self.is_running():
+            ran_for = time.time() - self.on_since
+            if self.runtime + ran_for >= self.desired_runtime:
+                self.turn_off()
 
-        # If it does, since when
-
-        # Cumulate with current runtime
-
-        # Do we need to stop it ?
         print(f'Pump {self.name} updated')
--- tests/pump_test.py
+++ tests/pump_test.py
@@ -1,13 +1,31 @@
 import unittest
+import time
 
 from pump import Pump
 
 class TestPump(unittest.TestCase):
+    def get_time(self):
+        return self.emulated_time
+
+    def setUp(self):
+        # Define the emulated time
+        self.emulated_time = time.mktime(
+                time.strptime("2020-01-18 02:02:05", "%Y-%m-%d %H:%M:%S"))
+
+        # Register the time.time function and patch it then
+        self.time_method = time.time
+        time.time = self.get_time
+
+    def tearDown(self):
+        # Restore the time.time function
+        time.time = self.time_method
+
     def test_initial_values(self):
         pump = Pump('test_pump', 200, 3 * 3600)
         self.assertEqual(pump.name, 'test_pump')
         self.assertEqual(pump.power, 200)
         self.assertEqual(pump.desired_runtime, 3 * 3600)
+        self.assertIsNone(pump.on_since)
 
     def test_can_run_with_enough_power(self):
         pump = Pump('test_pump', 200, 3 * 3600)
@@ -18,3 +36,19 @@ class TestPump(unittest.TestCase):
     def test_should_run_until_desired(self):
         pump = Pump('test_pump', 200, 10)
         self.assertTrue(pump.should_run())
+        pump.runtime = 10
+        self.assertFalse(pump.should_run())
+
+    def test_turn_on_stores_time(self):
+        pump = Pump('test_pump', 200, 10)
+        pump.turn_on()
+        self.assertEqual(pump.on_since, self.emulated_time)
+
+    def test_turn_off_updates_runtime(self):
+        pump = Pump('test_pump', 200, 10)
+        pump.turn_on()
+        self.assertEqual(pump.on_since, self.emulated_time)
+        self.emulated_time += 20
+        pump.turn_off()
+        self.assertIsNone(pump.on_since)
+        self.assertEqual(pump.runtime, 20)
