0b106c1fd2dd87b77068db70fcf6a903b8081076
Julien Moutte <655311+jmoutte@users.noreply.github.com>
2021-11-09 08:28:52 +0100

    Temporary workaround for main logic.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from unittest.main import main
 from envoy import Envoy
 from pump import Pump
 
@@ -31,26 +32,31 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 device = Envoy('192.168.10.12')
-pumps = []
-config = [ { 'name': 'Main', 'power': 1000, 'desired_runtime': 4 * 3600, 'GPIO_ID': 8 },
-           { 'name': 'Polaris', 'power': 1000, 'desired_runtime': 1 * 3600, 'GPIO_ID': 10 } ]
 
-for c in config:
-    pumps.append(Pump(c['name'], c['power'], c['desired_runtime'], c['GPIO_ID']))
+main_pump = Pump('Main', 1650, 3 * 3600, 8)
+aux_pump = Pump('Polaris', 1100, 0.5 * 3600, 10)
 
-# FIXME
-pumps[1].chain(pumps[0])
+aux_pump.chain(main_pump)
+
+pumps = [main_pump, aux_pump]
 
 while True:
     for p in pumps:
-        p.update()
+        p.update() # Potentially stops a pump that reached desired runtime, update counters
     availability = device.update()
-    for p in pumps:
-        start = False
-        if p.should_run():
-            start, availability = p.can_run(availability)
-        if start:
-            p.turn_on()
-        else:
-            p.turn_off()
+    if availability <= 0:
+        if aux_pump.is_running():
+            aux_pump.turn_off()
+            del device.consumption
+        elif main_pump.is_running():
+            main_pump.turn_off()
+            del device.consumption
+    else:
+        for p in pumps:
+            start = False
+            if p.should_run():
+                start, availability = p.can_run(availability)
+            if start:
+                p.turn_on()
+                del device.consumption
     sleep(60)
