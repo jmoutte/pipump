e4764bde23717da16b2a7a90421ed8bc013a8e3c
Julien Moutte <655311+jmoutte@users.noreply.github.com>
2021-11-09 08:27:21 +0100

    Improve moving average with deleter.

--- pvsystem.py
+++ pvsystem.py
@@ -2,22 +2,34 @@ from collections import deque
 from statistics import mean
 
 class PVSystem:
-    _consumption_readings = deque([0], maxlen=5)
-    _production_readings = deque([0], maxlen=5)
+    _consumption_readings = deque([], maxlen=5)
+    _production_readings = deque([], maxlen=5)
 
     @property
     def consumption(self):
+        if not len(self._consumption_readings):
+            return 0
         return mean(self._consumption_readings)
     
     @consumption.setter
     def consumption(self, value):
         self._consumption_readings.append(value)
+    
+    @consumption.deleter
+    def consumption(self):
+        self._consumption_readings.clear()
 
     @property
     def production(self):
+        if not len(self._production_readings):
+            return 0
         return mean(self._production_readings)
     
     @production.setter
     def production(self, value):
         self._production_readings.append(value)
+    
+    @production.deleter
+    def production(self):
+        self._production_readings.clear()
         
--- tests/pvsystem_test.py
+++ tests/pvsystem_test.py
@@ -8,6 +8,13 @@ class TestPVSystem(unittest.TestCase):
         self.assertEqual(pvsystem.consumption, 0)
         self.assertEqual(pvsystem.production, 0)
 
+    def test_set_values_first(self):
+        pvsystem = PVSystem()
+        pvsystem.production = 100
+        pvsystem.consumption = 100
+        self.assertEqual(pvsystem.consumption, 100)
+        self.assertEqual(pvsystem.production, 100)
+
     def test_set_values_moving_average(self):
         pvsystem = PVSystem()
         for c in [0, 100, 200, 100, 0]:
@@ -16,3 +23,15 @@ class TestPVSystem(unittest.TestCase):
             pvsystem.production = p
         self.assertEqual(pvsystem.consumption, 80)
         self.assertEqual(pvsystem.production, 80)
+
+    def test_del_clears_moving_average(self):
+        pvsystem = PVSystem()
+        for i in [0, 100, 200, 100, 0]:
+            pvsystem.consumption = i
+            pvsystem.production = i
+        self.assertEqual(pvsystem.consumption, 80)
+        self.assertEqual(pvsystem.production, 80)
+        del pvsystem.production
+        del pvsystem.consumption
+        self.assertEqual(pvsystem.consumption, 0)
+        self.assertEqual(pvsystem.production, 0)
